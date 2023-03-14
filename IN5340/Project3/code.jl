dfa9e00446ec971090b2dcb7d74c2124e2da425b
Simon Andreas Bjørn <s.a.bjorn@fys.uio.no>
2023-03-14 13:56:11 +0100

    Upload and stop project 3

--- IN5340/Project3/code.jl
+++ IN5340/Project3/code.jl
@@ -4,7 +4,7 @@ using CairoMakie
 nord3 = "#4c566a"
 nord4 = "#d8dee9"
 set_theme!(Theme(
-    font_size = 25,
+    fontsize = 25,
     Axis = (
         backgroundcolor = :transparent,
         xtickcolor = nord4, ytickcolor = nord4,
@@ -19,7 +19,7 @@ set_theme!(Theme(
 
 # Test plot
 F = Figure(backgroundcolor=:transparent)
-a = Axis(F[1,1], title="Nord Test")
+Axis(F[1,1], title="Nord Test")
 lines!(0:0.01:2pi, sin.(0:0.01:2pi))
 lines!(0:0.01:2pi, cos.(0:0.01:2pi))
 F
@@ -29,34 +29,38 @@ save("test.pdf", F)
 ##########################
 # Imports & Preamble 
 ##########################
-using Unitful, MAT, DSP, FFTW
-import Unitful: Hz, m, s
-
-toUnit(unit) = x -> uconvert(unit, x)
+using MAT, DSP, FFTW
+using Unitful
 
 sonardata = matread("sonardata4.mat")
 
-B     = sonardata["B"]    * Hz
-fs    = sonardata["fs"]   * Hz
-fc    = sonardata["fc"]   * Hz
-T_p   = sonardata["T_p"]  * s
+B     = sonardata["B"]      * Hz
+fs    = sonardata["fs"]     * Hz
+fc    = sonardata["fc"]     * Hz
+T_p   = sonardata["T_p"]    * s
 data  = sonardata["data"]
-α     = B / T_p 
+α     = B / T_p
 
-s_Tx(t) = exp.(1im*2π*α*(t.^2) ./ 2) .* (abs.(t) .< T_p/2)
+s_Tx(t::Time) = exp.(1im*2π*α*(t.^2) ./ 2) .* (abs.(t) .< T_p/2)
 
 ##########################
 # 1
 ##########################
 
 N = 1024
-arr = data[1200:1200+N, 14]
+arr = data[1200:1200+N-1, 14]
 
-PSD = fft ∘ fftshift ∘ x->abs.(x.*conj(x)).^2 ./ length(x)
+PSD(x) = fftshift(fft(x)) |> x->abs.(x.*conj(x)).^2 / N .|> amp2db
 
-Farr = fftshift(fft(arr))
-Pₓₓ = abs.(Farr.*conj(Farr)).^2 / N
-lines(abs.(Pₓₓ))
+Ω = range(-fs/2,fs/2,N)
+Pₓₓ = PSD(arr)
 
-win = kaiser(N, 2.2) |> x->x/sum(x)
-Pₓₓ2 = 
+F = Figure(backgroundcolor=:transparent)
+Axis(
+    F[1,1], 
+    title="Periodogram of channel 14, samples 1200:+1024",
+    xlabel="Frequency [Hz]",
+    ylabel="Power [dB]"
+)
+lines!(Ω[Ω .>= 0], Pₓₓ[Ω .< 0])
+F
